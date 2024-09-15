4b609ea28cdb7d9e9ec023d7bb013540f25e7253
ChikatoTakenouchi <maxmaxastea@gmail.com>
2024-09-15 16:43:01 +0900

    依存関係を調整した

--- HashtagCatcherPackage/Package.swift
+++ HashtagCatcherPackage/Package.swift
@@ -3,6 +3,10 @@
 
 import PackageDescription
 
+private extension PackageDescription.Target.Dependency {
+    static let realm: Self = .product(name: "RealmSwift", package: "realm-swift")
+}
+
 private let features: [PackageDescription.Target.Dependency] = ["FindFeature", "GroupFeature", "SettingsFeature"]
 
 let package = Package(
@@ -15,6 +19,10 @@ let package = Package(
         .library(
             name: "Staging",
             targets: ["StagingApp"]),
+    ],
+    dependencies: [
+        //　本当は20.0.0を使いたいが、うまく入らない
+        .package(url: "https://github.com/realm/realm-swift.git", from: "10.53.1")
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
@@ -31,15 +39,40 @@ let package = Package(
         ),
         .target(
             name: "FindFeature",
+            dependencies: ["UIComponents", "UseCases"],
             path: "./Sources/Features/Find"
         ),
         .target(
             name: "GroupFeature",
+            dependencies: ["UIComponents", "UseCases"],
             path: "./Sources/Features/Group"
         ),
         .target(
             name: "SettingsFeature",
+            dependencies: ["UIComponents", "UseCases"],
             path: "./Sources/Features/Settings"
+        ),
+        .target(
+            name: "UIComponents",
+            path: "./Sources/Core/UIComponents"
+        ),
+        .target(
+            name: "UseCases",
+            dependencies: ["Models", "Repositories"],
+            path: "./Sources/Core/UseCases"
+        ),
+        .target(
+            name: "Models",
+            path: "./Sources/Core/Models"
+        ),
+        .target(
+            name: "Repositories",
+            dependencies: [.realm, "APIService"],
+            path: "./Sources/Core/Repositories"
+        ),
+        .target(
+            name: "APIService",
+            path: "./Sources/Core/APIService"
         )
     ]
 )
